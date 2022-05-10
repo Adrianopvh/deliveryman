1fde38c8bf94f254bdae1865ef94bee5bd1e5b4a
Adrianopvh <adriano.rodriques12389@gmail.com>
2022-05-10 16:15:59 -0400

    Autenticação com token

---
+++ src/middlewares/ensureAuthenticateClient.ts
@@ -0,0 +1,35 @@
+import { NextFunction, Request, Response } from "express";
+import { verify } from "jsonwebtoken";
+
+interface IPayload {
+    sub: string;
+}
+
+
+export async function ensureAuthenticateClient(request: Request, response: Response, next: NextFunction) {
+    const authHeader = request.headers.authorization;
+
+    if(!authHeader) {
+        return response.status(401).json({
+            message: "Token missing",
+        });
+    }
+
+    // Bearer 5454544646A4A4S4-AS4A4S6D6AS5D
+    // [0] - Bearer
+    // [1] - 5454544646A4A4S4-AS4A4S6D6AS5D
+
+    const [, token ] = authHeader.split(" ")
+
+    try {
+        const { sub } = verify(token, "6ee6213d830b4f6e13aaf4c5c8697a98") as IPayload;
+
+        request.id_client = sub;
+
+        return next();
+    } catch(err) {
+        return response.status(401).json({
+            message: "Invalid token!",
+        });
+    }
+}
